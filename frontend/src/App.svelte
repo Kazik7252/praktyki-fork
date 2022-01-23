da2f73fe9fc32e9ac24b0bcc80e9b2b2d72f35a1
Krixlion <konstanty.cyran.k@gmail.com>
2022-01-24 00:43:35 +0100

    PlaceHolder

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -5,6 +5,7 @@ import NotFound from "./routes/NotFound.svelte";
 import Topics from "./routes/Topics.svelte";
 import Home from "./routes/Home.svelte";
 import Topic from "./routes/Topic.svelte";
+import User from "./routes/User.svelte";
 
 
 	let routes = {
@@ -12,6 +13,7 @@ import Topic from "./routes/Topic.svelte";
 		"/home": Home,
 		"/topics": Topics,
 		"/topic/:id": Topic,
+		"/user": User,
 
 		"*": NotFound
 	}
---
+++ frontend/src/routes/User.svelte
@@ -0,0 +1,31 @@
+<script>
+import { onMount } from "svelte";
+
+    const riot_key = 'RGAPI-101efe84-aaf5-4bf3-ab70-f5621d2c7140';
+
+    let username = 'krixlionpl';
+    let url = 'https://eun1.api.riotgames.com/lol/summoner/v4/summoners/by-name/'+username;
+
+    onMount(async () => {
+		try{
+		const response = await fetch(url, {
+            mode: 'cors',
+            headers: {
+                'Access-Control-Request-Method': "GET",
+                'Content-Type': 'application/json',
+                "X-Riot-Token": "RGAPI-101efe84-aaf5-4bf3-ab70-f5621d2c7140",
+            }
+        });
+		const data = await response.json();
+		console.log(data.username);
+		} catch(error){
+			console.log(error)
+		}
+	});
+
+</script>
+<div>
+    <!-- {#each data as data}
+        <h1>Username: {data.username}</h1>
+    {/each} -->
+</div>
